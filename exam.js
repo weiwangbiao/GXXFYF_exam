2d8d5b5ff9b9bb0bbd8b9556e43bbfae58ebc936
weiwangbiao <50125025+weiwangbiao@users.noreply.github.com>
2022-10-13 20:28:22 +0800

    Update exam.js

--- exam.js
+++ exam.js
@@ -1,33 +1,78 @@
-// @require    http://code.jquery.com/jquery-1.11.0.min.js
-
-if (window.location.host != 'exam.gxpf.cn') {
-	alert('Enter exam/exercise first, and then click me.');
-}
-else {
-	var input_time = prompt("Please input sleep time(s): ","20");
-	input_time = parseInt(input_time*1000);
-	var input_rndtime = prompt("Please input random time(s): ","20");
-	var now_id = 1;	
-	doit();
-	/*
-	if ($("#btnHandPaper").length > 0) {
-		$("#btnHandPaper").click();
-		}
-	*/
-}
+$("h4:first").after('  答题数：<input type="text" id="timus" style="width:34px;" value="100"> \
+间隔：<input type="text" id="sleep_time" style="width:26px;" value="10"> \
++随机<input type="text" id="rnd_time" style="width:26px;"  value="15">秒。 \
+<input type="button" value="答题！" id="answerme"/>  ')
 function doit(){
-	if(now_id < 51) {
-		var rndtime = Math.round(Math.random()*input_rndtime*1000);
-		sltime = input_time+rndtime
-		console.log(new Date().toLocaleTimeString()+': answering no.'+now_id+' ...and wait for '+sltime/1000+' seconds...');		
-		$("li[data-type='0'").click();
-		#document.getElementById("btnConfirm").click();
-		$("#btnConfirm").click();
-		if ($("#btnNext").length > 0){
-			$("#btnNext").click();
-			}
-		setTimeout("doit()", sltime+rndtime);
-		now_id++;
-	}	
+    var input_time = parseInt($("#sleep_time").val()*1000);
+    var rndtime = Math.random()*parseInt($("#rnd_time").val())*1000;
+    sltime = input_time+rndtime
+    // $("#answerme").attr("disabled","disabled");
+	// console.log(input_time);
+    // console.log(rndtime);
+    // console.log(sltime);
+    
+    if(now_id < parseInt($("#timus").val())+1) {
+		var nowSubjectId = $("#nowSubjectId").val();
+		//if (nowSubjectId == null ){nowSubjectId = '15236'}
+		var url = "https://raw.githubusercontent.com/weiwangbiao/GXXFYF_exam/master/tiku/"+nowSubjectId+".json";
+		var request = new XMLHttpRequest();
+		request.open("get",url);
+		request.send(null);
+		request.onload =  function(){
+			if (request.status == 200){
+				var json = JSON.parse(request.responseText);
+				answer_id =  json.id.split('_');
+				console.log($("#nowSubjectId").val()+": "+answer_id);
+                for (i=0; i<answer_id.length; i++){
+                    // console.log(answer_id[i]);
+					if  (json.itemIds.includes(answer_id[i])){
+						$("li[itemid='"+answer_id[i]+"']").click();
+						}else{
+							console.log('use data-type=0');
+							$("li[data-type='0']").click();
+							}
+					}
+				if ($(".primary").length < 1){
+					console.log('没有找到答案，乱选吧。');
+					$("li[data-type='0']").click();
+					}
+				console.log(new Date().toLocaleTimeString()+': 已经答完第'+ now_id+' 题，等待'+parseInt(sltime)/1000+' 秒答下一题...');		
+				if ($("#btnConfirm").length > 0){
+					$("#btnConfirm").click();
+					}
+				if ($("#btnNext").length > 0){
+					$("#btnNext").click();
+					console.log('next');
+					}
+				setTimeout("doit()", sltime);
+				now_id++;
+
+
+			}else{
+				console.log('use data-type=0');
+				$("li[data-type='0']").click();
+				console.log(new Date().toLocaleTimeString()+': 已经答完第'+ now_id+' 题，等待'+parseInt(sltime)/1000+' 秒答下一题...');		
+				if ($("#btnConfirm").length > 0){
+					$("#btnConfirm").click();
+					}
+				if ($("#btnNext").length > 0){
+					$("#btnNext").click();
+					console.log('next');
+					}
+				now_id++;
+				setTimeout("doit()", sltime);				
+				}
+		}
+	}else{
+		console.log(new Date().toLocaleTimeString()+': 答题完毕');
+		}
+	
+	// $("#answerme").removeAttr("disabled");
+	
 		
 }
+var now_id = 1;
+$("#answerme").click(()=>{
+	now_id = 1;
+    doit();  
+})
