56a685b14936d08e58f86974743e34a287b0abc0
weiwangbiao <50125025+weiwangbiao@users.noreply.github.com>
2022-10-29 23:09:16 +0800

    Update page_control.js

--- page_control.js
+++ page_control.js
@@ -2,3 +2,5 @@ $("#headerbar").remove();
 $("#siderBar").remove();
 $(".ml-lg").remove();
 $(".contents").removeClass("contents");
+$("#qrcode-box").remove();
+$(".login-wrapper").removeClass("login-wrapper");
