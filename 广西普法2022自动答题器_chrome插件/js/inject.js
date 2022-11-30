956ffe293340218e312185473f41c4b476921c8c
weiwangbiao <weiwangbiao@gmail.com>
2022-11-30 19:22:10 +0800

    test

--- 广西普法2022自动答题器_chrome插件/js/inject.js
+++ 广西普法2022自动答题器_chrome插件/js/inject.js
@@ -1,75 +1,79 @@
-$("h4:first").after('答题数：<input type="text" id="timus" style="width:34px;" value="100"> \
- 间隔：<input type="text" id="sleep_time" style="width:26px;" value="10"> \
+$("h4.d-flex-item").after('间隔：<input type="text" id="sleep_time" style="width:26px;" value="10"> \
 +随机<input type="text" id="rnd_time" style="width:26px;"  value="15">秒。 \
-<input type="button" value="答题！" id="answerme"/>  ')
-function doit(){
-    var input_time = parseInt($("#sleep_time").val()*1000);
-    var rndtime = Math.random()*parseInt($("#rnd_time").val())*1000;
-    sltime = input_time+rndtime
-    // $("#answerme").attr("disabled","disabled");
-    
-    if(now_id < parseInt($("#timus").val())+1) {
+<input type="button" value="答题！" id="answerme"/>   \
+<input type="button" value="停止！" id="stopme" style="display:none;"  \
+onclick= "clearTimeout(t1); "/>  ')
+function doit() {
+	var now_id = parseInt($(".ing").text());
+	if (!now_id) { now_id = 1 + parseInt($("#totalCount").text()); }
+	var input_time = parseInt($("#sleep_time").val() * 1000);
+	var rndtime = Math.random() * parseInt($("#rnd_time").val()) * 1000;
+	sltime = input_time + rndtime
+	$("#answerme").attr("style", "display:none;");
+	$("#stopme").attr("style", "display:block;");
+
+	if ($(".noanswer").length > 0 || $("#btnNext").length > 0) {
 		//获取当前题目的Id，拼接成获取答案的请求网址，用的是GitHub的库，避免重复请求考试服务器引发怀疑
 		var nowSubjectId = $("#nowSubjectId").val();
 		//if (nowSubjectId == null ){nowSubjectId = '15236'}
-		var url = "https://ghproxy.com/https://raw.githubusercontent.com/weiwangbiao/GXXFYF_exam/master/tiku/"+nowSubjectId+".json";
+		var url = "https://ghproxy.com/https://raw.githubusercontent.com/weiwangbiao/GXXFYF_exam/master/tiku/" + nowSubjectId + ".json";
 		var request = new XMLHttpRequest();
-		request.open("get",url);
+		request.open("get", url, true); //true异步，false同步，等待结果后才继续
 		request.send(null);
-		request.onload =  function(){
-			if (request.status == 200){
+		request.onload = function () {
+			if (request.status == 200) {
 				var json = JSON.parse(request.responseText);
-				answer_id =  json.id.split('_');
-				console.log($("#nowSubjectId").val()+": "+answer_id);
-                for (i=0; i<answer_id.length; i++){
-                    // console.log(answer_id[i]);
-					if  (json.itemIds.includes(answer_id[i])){
-						$("li[itemid='"+answer_id[i]+"']").click();
-						}else{
-							console.log('use data-type=0');
-							$("li[data-type='0']").click();
-							}
+				answer_id = json.id.split('_');
+				console.log($("#nowSubjectId").val() + ": " + answer_id);
+				for (i = 0; i < answer_id.length; i++) {
+					// console.log(answer_id[i]);
+					if (json.itemIds.includes(answer_id[i])) {
+						$("li[itemid='" + answer_id[i] + "']").click();
+					} else {
+						console.log('use data-type=0');
+						$("li[data-type='0']").click();
 					}
-				if ($(".primary").length < 1){
+				}
+				if ($(".primary").length < 1) {
 					console.log('没有找到答案，乱选吧。');
 					$("li[data-type='0']").click();
-					}
-				console.log(new Date().toLocaleTimeString()+': 已经答完第'+ now_id+' 题，等待'+parseInt(sltime)/1000+' 秒答下一题...');		
-				if ($("#btnConfirm").length > 0){
+				}
+				console.log(new Date().toLocaleTimeString() + ': 已经答完第' + now_id + ' 题，等待' + parseInt(sltime) / 1000 + ' 秒答下一题...');
+				if ($("#btnConfirm").length > 0) {
 					$("#btnConfirm").click();
-					}
-				if ($("#btnNext").length > 0){
+				}
+				if ($("#btnNext").length > 0) {
 					$("#btnNext").click();
-					}
-				setTimeout("doit()", sltime);
+				}
+				t1 = setTimeout("doit()", sltime);
 				now_id++;
 
 
-			}else{
+			} else {
 				//console.log('use data-type=0');
 				$("li[data-type='0']").click();
-				console.log(new Date().toLocaleTimeString()+': 已经答完第'+ now_id+' 题，等待'+parseInt(sltime)/1000+' 秒答下一题...');		
-				if ($("#btnConfirm").length > 0){
+				console.log(new Date().toLocaleTimeString() + ': 已经答完第' + now_id + ' 题，等待' + parseInt(sltime) / 1000 + ' 秒答下一题...');
+				if ($("#btnConfirm").length > 0) {
 					$("#btnConfirm").click();
-					}
-				if ($("#btnNext").length > 0){
+				}
+				if ($("#btnNext").length > 0) {
 					$("#btnNext").click();
 					console.log('next');
-					}
-				now_id++;
-				setTimeout("doit()", sltime);				
 				}
+
+				t1 = setTimeout("doit()", sltime);
+			}
 		}
-	}else{
-		console.log(new Date().toLocaleTimeString()+': 答题完毕');
-		}
-	
-	// $("#answerme").removeAttr("disabled");
-	
-		
+	} else {
+		console.log(new Date().toLocaleTimeString() + ': 答题完毕');
+		//用JavaScript实现自动点击由confirm弹出的对话框中的“确定”按钮
+		window.confirm = () => { return 1 };
+		$("#btnHandPaper").trigger("click");
+	}
 }
 var now_id = 1;
-$("#answerme").click(()=>{
-	now_id = 1;
-    doit();  
+$("#answerme").click(() => {
+	doit();
 })
+
+
